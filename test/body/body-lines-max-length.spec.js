481db57d007ae73e04201dcbf382f494af987832
Giovanni Garufi <g.garufi@students.uu.nl>
2018-07-12 17:53:14 +0200

    fix: Body-lines-max-length only applies to body
    Change-type: minor
    Signed-off-by: Giovanni Garufi <giovanni@resin.io>

--- test/body/body-lines-max-length.spec.js
+++ test/body/body-lines-max-length.spec.js
@@ -2,10 +2,14 @@ const ava = require('ava')
 const rules = require('../../rules')
 
 const validMessage = {
-  message: [ 'A'.repeat(72), 'B'.repeat(72), 'C'.repeat(72) ].join('\n')
+  body: [ 'A'.repeat(72), 'B'.repeat(72), 'C'.repeat(72) ].join('\n')
 }
 const invalidMessage = {
-  message: [ 'A'.repeat(72), 'B'.repeat(73), 'C'.repeat(72) ].join('\n')
+  body: [ 'A'.repeat(72), 'B'.repeat(73), 'C'.repeat(72) ].join('\n')
+}
+const longFooters = {
+  body: [ 'A'.repeat(72) ].join('\n'),
+  footers: [ 'A'.repeat(73) ]
 }
 const runTest = (test) => rules.bodyLinesMaxLength(test)
 
@@ -16,4 +20,8 @@ ava.test('body-lines-max-length: should accept valid message', (test) => {
 ava.test('body-lines-max-length: should reject invalid message.', (test) => {
   const error = test.throws(() => runTest(invalidMessage))
   test.is(error.message, 'No line in the body should exceed 72 characters')
+})
+
+ava.test('body-lines-max-length: should accept arbitrarily long footers', (test) => {
+  test.notThrows(() => runTest(longFooters))
 })
